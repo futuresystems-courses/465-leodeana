85e3f3ed14f52aed14672e4ce9347f76eb5db953
leodeana <leonardo.deana@ge.com>
2015-05-08 20:28:01 -0400

    Repository HW3 ajustments

--- leodeana_cloudmesh_ex1.py
+++
@@ -1,3 +0,0 @@
-# coding: utf-8
-import cloudmesh
-print cloudmesh.version()
--- leodeana_cloudmesh_ex2.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-import cloudmesh
-print cloudmesh.shell("cloud list")
-print cloudmesh.shell("cloud on india")
-print cloudmesh.shell("cloud list")
